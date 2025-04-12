05e4fea90ec4c95b306677c333a9a0d51ec6948a
Marcos-gitHub <marcoslopezsanchez@hotmail.com>
2025-04-12 09:32:15 -0600

    gitignore

---
+++ backend/src/main/java/com/enterprise/tecno/clone/CloneApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.tecno.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/enterprise/tecno/clone/CloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.tecno.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
